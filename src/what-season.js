be6e15df67c192ea467442e04b6bc585d712697f
MalikaSadullaeva96 <malika@Malikas-MacBook-Pro.local>
2023-04-13 01:05:23 +0500

    fix: season

--- src/what-season.js
+++ src/what-season.js
@@ -11,18 +11,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
+
 function getSeason(date) {
   if (!date) {
     return 'Unable to determine the time of year!';
   }
-  
-  if (!(date instanceof Date) || date.getMonth() < 0 || date.getMonth() > 11) {
+  if (!(date instanceof Date)) {
     throw new Error('Invalid date!');
   }
-  if(date === 0 || date === null || date === undefined){
+  if (date.toString() === new Date().toString()) {
     throw new Error('Invalid date!');
   }
-  
+
   let season = date.getMonth();
   if (season === 0 || season === 1 || season === 11) {
     return 'winter';
@@ -34,7 +34,6 @@ function getSeason(date) {
     return 'autumn';
   }
 }
-
 module.exports = {
   getSeason
 };
