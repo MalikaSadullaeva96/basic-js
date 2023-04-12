afee9e78a04101c763014410b4611156921bb679
MalikaSadullaeva96 <malika@Malikas-MacBook-Pro.local>
2023-04-12 22:17:48 +0500

    fix: transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -16,13 +16,14 @@ const { NotImplementedError } = require('../extensions/index.js');
 function transform(arr) {
   if (!Array.isArray(arr)) {
     throw new Error("'arr' parameter must be an instance of the Array!");
-  } 
+  }
   const transformedArr = [];
+
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === '--discard-next') {
       i++;
     } else if (arr[i] === '--discard-prev') {
-      if (transformedArr.length > 0) {
+      if (arr[i - 2] !== '--discard-next') {
         transformedArr.pop();
       }
     } else if (arr[i] === '--double-next') {
@@ -30,7 +31,7 @@ function transform(arr) {
         transformedArr.push(arr[i + 1]);
       }
     } else if (arr[i] === '--double-prev') {
-      if (i > 0) {
+      if (i > 0 && arr[i - 2] !== '--discard-next') {
         transformedArr.push(arr[i - 1]);
       }
     } else {
@@ -39,6 +40,7 @@ function transform(arr) {
   }
   return transformedArr;
 }
+
 
 module.exports = {
   transform
