475a79b201dec5c182261b812de2b5db9b5c6c3b
MalikaSadullaeva96 <malika@Malikas-MacBook-Pro.local>
2023-04-16 22:22:52 +0500

    feat:cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,65 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.encrypted = [];
+    this.alph=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+  
+    let keyIndex = 0;
+    this.encrypted = [];
+  
+    for (let i = 0; i < message.length; i++) {
+      const messageChar = message.charAt(i);
+      if (this.alph.includes(messageChar)) {
+        const keyChar = key.charAt(keyIndex % key.length);
+        const encryptedChar = this.alph[(this.alph.indexOf(messageChar) + this.alph.indexOf(keyChar)) % 26];
+        this.encrypted.push(encryptedChar);
+        keyIndex++;
+      } else {
+        this.encrypted.push(messageChar);
+      }
+    }
+  
+    if (!this.isDirect) {
+      this.encrypted.reverse();
+    }
+  
+    return this.encrypted.join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+  
+    let keyIndex = 0;
+    this.decrypted = [];
+  
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      const encryptedChar = encryptedMessage.charAt(i);
+      if (this.alph.includes(encryptedChar)) {
+        const keyChar = key.charAt(keyIndex % key.length);
+        const decryptedChar = this.alph[(this.alph.indexOf(encryptedChar) - this.alph.indexOf(keyChar) + 26) % 26];
+        this.decrypted.push(decryptedChar);
+        keyIndex++;
+      } else {
+        this.decrypted.push(encryptedChar);
+      }
+    }
+    if (!this.isDirect) {
+      this.decrypted.reverse();
+    }
+    return this.decrypted.join('');
   }
 }
 
